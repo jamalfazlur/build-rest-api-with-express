defb529da535dd9742cb5c49b1c8c0e50a39ebaf
Jamal Fazlur Rahman <fazlur.jamal@gmail.com>
2021-05-21 10:15:31 +0700

    add basic logging with morgan

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
+
+app.use(morgan("dev"));
 
 app.get("/", function(req, res){
   res.send("Hallo!");
 });
 
-app.listen(3000);
+app.listen(3000, function(){
+  console.log("app listening on port 3000");
+});
