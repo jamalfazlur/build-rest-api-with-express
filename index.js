c0f0f3e090af0dd9c348f16e631dd277ed1e65f8
Jamal Fazlur Rahman <fazlur.jamal@gmail.com>
2021-05-21 10:25:37 +0700

    add basic error handling

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ app.get("/", function(req, res){
   res.send("Hallo!");
 });
 
+app.get("/error", function(req, res){
+  throw new Error("Oops, i made a mistake!");
+});
+
+app.use(function(req, res){
+  res.status(400).send("Not found!");
+});
+
+app.use(function(err, req, res, next){
+  console.log(err.stack);
+  res.status(500).send(err.message);
+});
+
 app.listen(3000, function(){
   console.log("app listening on port 3000");
 });
